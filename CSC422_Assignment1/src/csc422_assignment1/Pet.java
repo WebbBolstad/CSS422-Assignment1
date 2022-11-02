dad942b94cb8050971fd224238afaf60643d5282
WebbBolstad <83431631+WebbBolstad@users.noreply.github.com>
2022-11-02 16:28:36 -0500

    Added Pet class that holds name and age of a pet

---
+++ CSC422_Assignment1/src/csc422_assignment1/Pet.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csc422_assignment1;
+
+/**
+ *
+ * @author webbb
+ */
+public class Pet {
+    
+    private String name;
+    private int age;
+    
+    public Pet(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {return name;}
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {this.name = name;}
+
+    /**
+     * @return the age
+     */
+    public int getAge() {return age;}
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(int age) {this.age = age;}
+}
