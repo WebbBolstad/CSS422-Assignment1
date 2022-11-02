30956ab593060c6038db8f35a2f885199ca19506
WebbBolstad <83431631+WebbBolstad@users.noreply.github.com>
2022-11-02 17:28:26 -0500

    Added addPet() method to main class

--- CSC422_Assignment1/src/csc422_assignment1/CSC422_Assignment1.java
+++ CSC422_Assignment1/src/csc422_assignment1/CSC422_Assignment1.java
@@ -5,17 +5,59 @@
  */
 package csc422_assignment1;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author webbb
  */
 public class CSC422_Assignment1 {
+    
+    private static ArrayList<Pet> petList = new ArrayList<>();
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        mainMenu();
     }
-    
+
+    private static void mainMenu() {
+        try (Scanner kb = new Scanner(System.in)) {
+            System.out.printf("What would you like to do? \n"
+                    + "1) View all pets \n"
+                    + "2) Add more pets \n"
+                    + "3) Exit Program \n");
+            
+            int choice = kb.nextInt();
+            
+            switch(choice){
+                case 1: viewAllPets();
+                case 2: addPet();
+                case 3: break;
+            }
+        }
+    }
+
+    private static void viewAllPets() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static void addPet() {
+        try (Scanner kb = new Scanner(System.in)){
+            boolean loop = true;
+            while (loop == true){
+                System.out.println("Add pet (name, age):");
+                String petName = kb.next();
+                int petAge = Integer.parseInt(kb.next());
+                
+                if (petName.equalsIgnoreCase("done")){ 
+                    loop = false;
+                    break;}
+                petList.add(new Pet(petName, petAge));
+            }
+        }
+        mainMenu();
+    }
 }
